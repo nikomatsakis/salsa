776daf32dd25ae1bdfda314b414f2f0c4d4b25f7
Niko Matsakis <niko@alum.mit.edu>
2021-12-05 05:16:10 -0500

    more wip

--- components/salsa-entity-macros/src/configuration.rs
+++ components/salsa-entity-macros/src/configuration.rs
@@ -1,13 +1,13 @@
 use syn::ImplItemMethod;
 
 pub(crate) struct Configuration {
-    jar_ty: syn::Type,
-    key_ty: syn::Type,
-    value_ty: syn::Type,
-    cycle_strategy: CycleRecoveryStrategy,
-    memoize_value: bool,
-    execute_fn: syn::ImplItemMethod,
-    recover_fn: syn::ImplItemMethod,
+    pub(crate) jar_ty: syn::Type,
+    pub(crate) key_ty: syn::Type,
+    pub(crate) value_ty: syn::Type,
+    pub(crate) cycle_strategy: CycleRecoveryStrategy,
+    pub(crate) memoize_value: bool,
+    pub(crate) execute_fn: syn::ImplItemMethod,
+    pub(crate) recover_fn: syn::ImplItemMethod,
 }
 
 pub(crate) enum CycleRecoveryStrategy {
