b4e26b60b1e2795f44db081a29018e63098c01fd
Niko Matsakis <niko@alum.mit.edu>
2021-12-27 07:58:34 -0500

    make DebugWithDb support into_debug

--- components/salsa-entity-mock/src/debug.rs
+++ components/salsa-entity-mock/src/debug.rs
@@ -1,21 +1,40 @@
 pub trait DebugWithDb<Db: ?Sized> {
-    fn debug<'me>(&'me self, db: &'me Db) -> DebugWith<'me, Self, Db> {
-        DebugWith { value: self, db }
+    fn debug<'me>(&'me self, db: &'me Db) -> DebugWith<'me, Db> {
+        DebugWith {
+            value: Box::new(self),
+            db,
+        }
+    }
+
+    fn into_debug<'me>(self, db: &'me Db) -> DebugWith<'me, Db>
+    where
+        Self: Sized + 'me,
+    {
+        DebugWith {
+            value: Box::new(self),
+            db,
+        }
     }
 
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result;
 }
 
-pub struct DebugWith<'me, Value: ?Sized, Db: ?Sized> {
-    value: &'me Value,
+impl<Db: ?Sized, T> DebugWithDb<Db> for &T
+where
+    T: DebugWithDb<Db> + ?Sized,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &Db) -> std::fmt::Result {
+        T::fmt(self, f, db)
+    }
+}
+
+pub struct DebugWith<'me, Db: ?Sized> {
+    value: Box<dyn DebugWithDb<Db> + 'me>,
     db: &'me Db,
 }
 
-impl<V: ?Sized, D: ?Sized> std::fmt::Debug for DebugWith<'_, V, D>
-where
-    V: DebugWithDb<D>,
-{
+impl<D: ?Sized> std::fmt::Debug for DebugWith<'_, D> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        DebugWithDb::fmt(self.value, f, self.db)
+        DebugWithDb::fmt(&*self.value, f, self.db)
     }
 }
