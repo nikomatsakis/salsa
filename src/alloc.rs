33ecba4f6446cfa3f220b06eb905e2c49d7a6020
Niko Matsakis <niko@alum.mit.edu>
2024-06-29 08:18:53 -0400

    generalize Alloc to permit `?Sized` values
    Didn't wind up using this but it seems good.

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,12 +1,15 @@
 use std::ptr::NonNull;
 
 /// A box but without the uniqueness guarantees.
-pub struct Alloc<T> {
+pub struct Alloc<T: ?Sized> {
     data: NonNull<T>,
 }
 
-impl<T> Alloc<T> {
-    pub fn new(data: T) -> Self {
+impl<T: ?Sized> Alloc<T> {
+    pub fn new(data: T) -> Self
+    where
+        T: Sized,
+    {
         let data = Box::new(data);
         let data = Box::into_raw(data);
         Alloc {
@@ -27,7 +30,16 @@ impl<T> Alloc<T> {
     }
 }
 
-impl<T> Drop for Alloc<T> {
+impl<T: ?Sized> From<Box<T>> for Alloc<T> {
+    fn from(data: Box<T>) -> Self {
+        let data = Box::into_raw(data);
+        Alloc {
+            data: unsafe { NonNull::new_unchecked(data) },
+        }
+    }
+}
+
+impl<T: ?Sized> Drop for Alloc<T> {
     fn drop(&mut self) {
         let data: *mut T = self.data.as_ptr();
         let data: Box<T> = unsafe { Box::from_raw(data) };
@@ -35,6 +47,6 @@ impl<T> Drop for Alloc<T> {
     }
 }
 
-unsafe impl<T> Send for Alloc<T> where T: Send {}
+unsafe impl<T: ?Sized> Send for Alloc<T> where T: Send {}
 
-unsafe impl<T> Sync for Alloc<T> where T: Sync {}
+unsafe impl<T: ?Sized> Sync for Alloc<T> where T: Sync {}
