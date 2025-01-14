9c697f3c2a87e637ea60729b9b19a637d722555e
Niko Matsakis <nikomat@amazon.com>
2025-01-14 20:56:59 +0000

    WIP serialization

--- components/salsa-macros/src/tracked_fn.rs
+++ components/salsa-macros/src/tracked_fn.rs
@@ -44,6 +44,8 @@ impl crate::options::AllowedOptions for TrackedFn {
     const LRU: bool = true;
 
     const CONSTRUCTOR_NAME: bool = false;
+    
+    const SERIALIZE: bool = true;
 }
 
 struct Macro {
--- components/salsa-macros/src/tracked_struct.rs
+++ components/salsa-macros/src/tracked_struct.rs
@@ -50,6 +50,8 @@ impl crate::options::AllowedOptions for TrackedStruct {
     const LRU: bool = false;
 
     const CONSTRUCTOR_NAME: bool = true;
+
+    const SERIALIZE: bool = true;
 }
 
 impl SalsaStructAllowedOptions for TrackedStruct {
